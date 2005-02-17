b66c8f78e4b8a5cadabf589e1e184f77a5b21345
strib <strib>
2005-02-17 16:59:34 +0000

    lots more graphing stuff

--- make-graph.pl
+++ make-graph.pl
@@ -5,10 +5,11 @@ use strict;
 # noise margin
 my $MARGIN = .1;
 
-my $XMAX = 3.1;
-my $XMIN = -3.1;
+my $XMAX = int rand 100 + 10;
+my $XMIN = $XMAX - int rand 200;
 
-my $NUM_POINTS = 1000;
+my $NUM_POINTS_SCATTER = 1000;
+my $NUM_POINTS_CURVE = (int rand 100) + 10;
 
 sub add_noise {
 
@@ -19,25 +20,96 @@ sub add_noise {
 
 }
 
-my $func = `./scigen.pl -f functions.in -s EXPR`;
-#print "func = $func\n";
+my $graph = `perl scigen.pl -f scirules.in -s GNUPLOT`;
+#print "# [$$] func = $func\n";
 
-for( my $j = 0; $j < $NUM_POINTS; $j++ ) {
+my @graph_lines = split( /\n/, $graph );
 
-    my $x = (rand ($XMAX-$XMIN))+$XMIN;
-    my $expr = $func . " ";
-    $expr =~ s/xxx/$x/g;
-    #print "expr = $expr\n";
-    my $y = eval( $expr );
-    if( !defined $y ) {
-	print "UNDEF!\n";
-	exit -1;
+my $type;
+my $curves;
+
+my $tmp_dir = "/tmp/scigengraph.";
+my $gpfile = "$tmp_dir$$.gnuplot";
+my $epsfile = "$tmp_dir$$.eps";
+my $datafile = "$tmp_dir$$.dat";
+my @labels = ();
+my $num_points = 10;
+
+open( GPFILE, ">$gpfile" ) or die( "Couldn't write to $gpfile" );
+
+print GPFILE "set terminal postscript eps\n";
+print GPFILE "set output \"$epsfile\"\n";
+
+foreach my $line (@graph_lines) {
+
+    if( $line =~ /graphtype (.*)=(.*)/ ) {
+	$type = $1;
+	$curves = $2;
+	if( $type eq "scatter" ) {
+	    $num_points = $NUM_POINTS_SCATTER;
+	} else {
+	    $num_points = $NUM_POINTS_CURVE;
+	}
+    } elsif( $line =~ /curvelabel (.*)/ ) {
+	push @labels, $1;
+    } else {
+	print GPFILE "$line\n";
     }
-
-    my $yn = &add_noise($y);
-
-    print "$x $yn\n";
 
 }
 
+my @x = ();
+for( my $j = 0; $j < $num_points; $j++ ) {
+    
+    my $x = (rand ($XMAX-$XMIN))+$XMIN;
+    push @x, $x;
+
+}
+@x = sort { $a <=> $b} @x;
+
+print GPFILE "plot ";
+for( my $i = 0; $i < $curves; $i++ ) {
+    my $label = $labels[$i];
+    print GPFILE "\'$datafile.$i\' $label with ";
+    if( $type eq "scatter" ) {
+	print GPFILE "points";
+    } else {
+	print GPFILE "linespoints";
+    }
+
+    if( $i != $curves -1 ) {
+	print GPFILE ", ";
+    } else {
+	print GPFILE "\n";
+    }
+
+    my $func = `perl scigen.pl -f functions.in -s EXPR`;
+
+    open( DAT, ">$datafile.$i" ) or die( "Couldn't write to $datafile.$i" );
+
+    foreach my $x (@x) {
+	
+	my $expr = $func . " ";
+	$expr =~ s/xxx/$x/g;
+	#print "expr = $expr\n";
+	my $y = eval( $expr );
+	if( !defined $y ) {
+	    next;
+	}
+	
+	my $yn = &add_noise($y);
+	
+	print DAT "$x $yn\n";
+	
+    }
+
+    close( DAT );
+
+}
+
+close( GPFILE );
+
+system( "gnuplot $gpfile" ) and die( "Couldn't gnuplot $gpfile" );
+system( "gv $epsfile" ) and die( "Couldn't gv $epsfile" );
+system( "rm $tmp_dir$$*" ) and die( "Couldn't rm anything" );
 
